9df19a47709cca21d60b5830872b5167f3cecc30
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-05-26 14:39:58 +0100

    created new and create function on post controller, set view for new post, added strong parametes, routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/posts", to: "posts#index"
+  get "/posts/new", to: "posts#new"
+  post "posts", to: "posts#create"
 end
