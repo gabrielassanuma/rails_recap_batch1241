50ae72781b45a391481e4d2ba1fb085f91ba1a21
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-05-27 13:46:37 +0100

    added update routes, included edit and update functions on post controller, applied DRY to post simple form, changed new.html.erb, added edit.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "/posts", to: "posts#index"
-  get "/posts/new", to: "posts#new"
-  post "posts", to: "posts#create"
+  root "posts#index"
+  get "/posts", to: "posts#index", as: "post_index"
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "posts", to: "posts#create", as: "posts" 
+  get "posts/:id", to:"posts#show", as: "post"
+  get "posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "posts/:id", to: "posts#update"
+
 end
